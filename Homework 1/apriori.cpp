32d5812ce20bf90d367771d367defde1ba406c1f
Saksham Rastogi <saksham@pop-os.localdomain>
2021-09-01 19:40:09 +0530

    Updated Code

--- Homework 1/apriori.cpp
+++ Homework 1/apriori.cpp
@@ -1,231 +1,191 @@
 /*
     It is not
-    allowed to parse the complete input data and save it into an array or similar data
-    structure. However, the frequent patterns and candidate sets can be stored in memory.
+    allowed to parse the complete input data and save it into an array or
+   similar data structure. However, the frequent patterns and candidate sets can
+   be stored in memory.
 */
 
 /*
     References :-
     1. https://root-forum.cern.ch/t/reading-data-file-in-c-code/17126/2
-    2. 
 */
 
+// TODO :- Sort the output lexographically before output.
+
+#include <algorithm>
+#include <cmath>
 #include <cstdio>
 #include <cstring>
 #include <fstream>
-#include <sstream>
-#include <cmath>
-#include <vector>
-#include <iostream>
 #include <iomanip>
-#include <algorithm>
-#include <queue>
+#include <iostream>
 #include <list>
+#include <map>
 #include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
 #include <stack>
 #include <unordered_map>
-#include <map>
-#include <set>
+#include <vector>
 
 using namespace std;
 
-// struct candidate {
-//     set<int> itemset;
-//     int count;
-// };
+#define sz(a) int((a).size())
+#define all(a) x.begin(), x.end()
+#define pb push_back
+#define endl '\n'
+#define watch(x) cout << #x << " : " << x << endl;
 
 map<int, int> mp;
-// vector<candidate> freq_sets, freq_sets_prev;
+// vector<int> cnts;
+map<int, int> cnts;
+vector<vector<int>> freq_sets, candidates;
+int support;
+const double threshold = 25;
 
-vector<vector<int>> freq_sets, freq_sets_prev, candidates;
-vector<int> cnt_sets, cnt_sets_prev;
+void init_pass() {
 
-/*
-    1. Generate candidate itemsets in order of length.
-    2. Tests each such candidate itemset for support threshold.
-    3. Uses all frequent itemsets of a particular length to generate candidates having length one more.
-*/
+  int dataset_size = 0;
 
-//! NOTE :- can try unordered_map instead of map.
+  freq_sets.clear();
 
-// Generate the initial candidate itemset C1 and F1 from that C1.
-/*
-    * Checked Working Fine.
-    @param support threshold.
-*/
-void init_pass (int support) {
-
-    mp.clear();
-
-    ifstream inputFile ("/home/saksham/Desktop/DataMiningAssignments/Homework 1/temp.dat");
-    string transaction;
-    int item;
-    while (getline(inputFile, transaction)) {
-        if (!transaction.length()) continue;
-        istringstream iss(transaction);
-        while (iss >> item) {
-            mp[item]++;
-        }
+  ifstream inputFile(
+      "/home/saksham/Desktop/DataMiningAssignments/Homework 1/webdocs.dat");
+  string transaction;
+  int item;
+  while (getline(inputFile, transaction)) {
+    if (!transaction.length())
+      continue;
+    dataset_size++;
+    istringstream iss(transaction);
+    while (iss >> item) {
+      mp[item]++;
     }
+  }
 
-    // for (auto it : mp) {
-    //     cout<<it.first<<"   "<<it.second<<endl;
-    // }
+  support = ceil(threshold * 0.01 * dataset_size);
 
-    for (auto it : mp) {
-        if (it.second >= support) {
-            vector<int> temp;
-            temp.push_back(it.first);
-            freq_sets.push_back(temp);
-        }
+  for (auto it : mp) {
+    if (it.second >= support) {
+      vector<int> temp{it.first};
+      freq_sets.pb(temp);
     }
-
-    // for (auto can : freq_sets) {
-    //     for (auto x : can.itemset) cout<<x<<" ";
-    //     cout<<can.count<<" ";
-    //     cout<<endl;
-    // }
-
+  }
 }
 
-
-bool can_merge (vector<int> f1, vector<int> f2) {
-    int num_items = f1.size(), cnt = 0;
-    bool ok = true;
-    auto it1 = f1.begin();
-    auto it2 = f2.begin();
-    while (ok && cnt < num_items-1) {
-        if (*it1 != *it2) {
-            ok = false;
-            break;
-        }
-        it1++;
-        it2++;
-        cnt++; 
+bool can_merge(int i, int j) {
+  int num_items = sz(freq_sets[i]), cnt = 0;
+  bool ok = true;
+  auto it1 = freq_sets[i].begin();
+  auto it2 = freq_sets[j].begin();
+  while (ok && cnt < num_items - 1) {
+    if (*it1 != *it2) {
+      ok = false;
+      break;
     }
-    if (!ok) return false;
-    if (*it1 >= *it2) return false;
-    return true;
+    it1++;
+    it2++;
+    cnt++;
+  }
+  if (!ok)
+    return false;
+  if (*it1 >= *it2)
+    return false;
+  return true;
 }
 
-bool subset (vector<int>& merged, int to_remove) {
-
-    vector<int> to_check;
-    copy_if(merged.begin(), merged.end(), back_inserter(to_check), [to_remove](const int& t) { return t != to_remove; });
-
-    bool ok = false;
-    for (auto freq : freq_sets_prev) {
-        if (freq == to_check) {
-            ok = true;
-            break;
-        }
+bool subset(vector<int> &merged, int to_remove) {
+  vector<int> to_check;
+  copy_if(merged.begin(), merged.end(), back_inserter(to_check),
+          [to_remove](const int &t) { return t != to_remove; });
+  bool ok = false;
+  for (auto freq : freq_sets) {
+    if (freq == to_check) {
+      ok = true;
+      break;
     }
-
-    return ok;
+  }
+  return ok;
 }
 
 // Generate the candidate C(k) from F(k-1)
-void candidate_gen () {
-    candidates.clear();
-    freq_sets_prev = freq_sets;
-    if (freq_sets_prev.size() == 0) {
-        return;
-    }
-    for (int i=0;i<freq_sets_prev.size();i++) {
-        for (int j=0;j<freq_sets_prev.size();j++) {
-            if (i == j) continue;
-            vector<int> f1 = freq_sets_prev[i];
-            vector<int> f2 = freq_sets_prev[j];
-            // The conditions are satisfied for merging.
-            bool merge = can_merge(f1, f2);
-            if (merge) {
-
-                vector<int> merged = f1;
-                merged.push_back(f2[f2.size()-1]);
-
-                // Do a subset check now.
-                bool ok = true;
-                for (auto it : merged) {
-                    ok = ok & subset(merged, it);
-                }
-
-                if (ok) {
-                    // for (auto x : merged) cout<<x<<" ";cout<<endl;
-                    candidates.push_back(merged);
-                }
-                // cout<<*(f1.begin())<<" "<<*(f2.begin())<<endl;
-            }
+void candidate_gen() {
+  candidates.clear();
+  if (sz(freq_sets) == 0)
+    return;
+  int n = sz(freq_sets);
+  for (int i = 0; i < n; i++) {
+    for (int j = i + 1; j < n; j++) {
+      bool merge = can_merge(i, j);
+      if (merge) {
+        vector<int> merged = freq_sets[i];
+        merged.pb(freq_sets[j][sz(freq_sets[j]) - 1]);
+        // Do a subset check now.
+        bool ok = true;
+        for (auto it : merged) {
+          ok = ok & subset(merged, it);
         }
+        if (ok) {
+          // for (auto x : merged) cout<<x<<" ";cout<<endl;
+          candidates.push_back(merged);
+        }
+      }
     }
+  }
 }
 
-void generate_frequent_itemsets (int support) {
+void generate_frequent_itemsets() {
+  freq_sets.clear();
+  cnts.clear();
+  // cnts.resize((candidates.size(), 0));
 
-    freq_sets.clear();
-    vector<int> cnts (candidates.size(), 0);
-
-    // Now check for every candidate in the database.
-
-    // Iterate through the transactions.
-    ifstream inputFile ("/home/saksham/Desktop/DataMiningAssignments/Homework 1/temp.dat");
-    string transaction;
-    int item;
-    vector<int> trans;
-    while (getline(inputFile, transaction)) {
-        if (!transaction.length()) continue;
-        istringstream iss(transaction);
-        while (iss >> item) {
-            trans.push_back(item);
-        }
-        // for (auto x : trans) cout<<x<<" ";cout<<endl;
-        for (int i=0;i<candidates.size();i++) {
-            bool ok = includes(trans.begin(), trans.end(), candidates[i].begin(), candidates[i].end());
-            if (ok) cnts[i]++;
-        }
-        trans.clear();
+  ifstream inputFile(
+      "/home/saksham/Desktop/DataMiningAssignments/Homework 1/webdocs.dat");
+  string transaction;
+  int item;
+  vector<int> trans;
+  while (getline(inputFile, transaction)) {
+    if (!transaction.length())
+      continue;
+    istringstream iss(transaction);
+    while (iss >> item) {
+      trans.push_back(item);
     }
-
-    // cout<<"----------------------------------------"<<endl;
-
-    for (int i=0;i<candidates.size();i++) {
-        if (cnts[i] >= support) {
-            freq_sets.push_back(candidates[i]);
-        }
-        // cout<<cnts[i]<<endl;
-        // for (auto x : candidates[i]) cout<<x<<" ";cout<<endl;
-    }   
+    for (int i = 0; i < candidates.size(); i++) {
+      bool ok = includes(trans.begin(), trans.end(), candidates[i].begin(),
+                         candidates[i].end());
+      if (ok)
+        cnts[i]++;
+    }
+    trans.clear();
+  }
+  for (int i = 0; i < candidates.size(); i++) {
+    if (cnts[i] >= support) {
+      freq_sets.push_back(candidates[i]);
+    }
+  }
 }
 
-int main (int argc, char *argv[]) {
+int main() {
 
-    // ifstream inputFile ("/home/saksham/Desktop/DataMiningAssignments/Homework 1/webdocs.dat");
-    // string line;
-    // int num;
-    // while (getline(inputFile, line)) {
-    //     if (!line.length()) continue;
-    //     istringstream iss(line);
-    //     while (iss >> num) {
-    //     }
-    // }
+  init_pass();
 
-    init_pass(10);   
+  for (auto v : freq_sets) {
+    for (auto x : v)
+      cout << x << " ";
+    cout << endl;
+  }
 
-    while (true) {
-        candidate_gen();
-        if (candidates.size() == 0) break;
-        generate_frequent_itemsets(10);
-        for (auto ff : freq_sets) {
-            for (auto x : ff) cout<<x<<" ";cout<<endl;
-        }
+  while (true) {
+    candidate_gen();
+    if (candidates.size() == 0)
+      break;
+    generate_frequent_itemsets();
+    for (auto v : freq_sets) {
+      for (auto x : v)
+        cout << x << " ";
+      cout << endl;
     }
- 
-    // for (auto ff : freq_sets) {
-    //     for (auto x : ff) cout<<x<<" ";cout<<endl;
-    // }
-
-    // for (auto cc : candidates) {
-    //     for (auto x : cc) cout<<x<<" ";cout<<endl;
-    // }
-
-    return 0;
+  }
 }
